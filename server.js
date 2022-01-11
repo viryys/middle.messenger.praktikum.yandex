c5df3c24300beb93322fe7d66efb9dd4fad2b727
Vadim Tatarchuk <tatarchuk.v.b@gmail.com>
2022-01-12 03:35:29 +0500

    подключил parcel, добавил папки в gitignore, настроит порт на 3000

---
+++ server.js
@@ -0,0 +1,12 @@
+// server.js
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(`${__dirname}/`));
+
+app.listen(PORT, function () {
+    console.log(`Example app listening on port ${PORT}!`);
+    console.log(__dirname)
+});
