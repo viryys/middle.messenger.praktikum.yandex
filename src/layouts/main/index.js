73b099587a54b9ac72a7830b1e7d62bd572e6c61
Vadim Tatarchuk <tatarchuk.v.b@gmail.com>
2022-02-04 03:32:46 +0500

    добавил основную страницу

--- src/layouts/main/index.js
+++ src/layouts/main/index.js
@@ -1,7 +1,20 @@
+import arrowRight from 'bundle-text:../../../static/img/arrow-right.svg';
+import zoom from 'bundle-text:../../../static/img/zoom.svg';
+import dot from 'bundle-text:../../../static/img/dot.svg';
+import clip from 'bundle-text:../../../static/img/clip.svg';
+import backSvg from 'bundle-text:../../../static/img/back.svg';
 import mainTemplate from './main.hbs';
+import styles from './main.css';
 
 const root = document.querySelector('#root');
 
-const data = {};
+const data = {
+  styles,
+  arrow: arrowRight,
+  zoom,
+  dot,
+  clip,
+  back: backSvg,
+};
 
 root.innerHTML = mainTemplate(data);
